00745edf778af30d612169ad9dde81381800a095
Sravan Kumar <sravankumar.g@techouts.com>
2022-12-27 20:03:25 +0530

    Removed unused tableview from workschedule

--- Growth99/MyProfile/Working Schedule/CustomTableViewCell/WorkingCustomTableViewCell.swift
+++ Growth99/MyProfile/Working Schedule/CustomTableViewCell/WorkingCustomTableViewCell.swift
@@ -13,7 +13,7 @@ protocol WorkingCellSubclassDelegate: AnyObject {
     func buttonWorkingtimeToTapped(cell: WorkingCustomTableViewCell)
 }
 
-class WorkingCustomTableViewCell: UITableViewCell, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+class WorkingCustomTableViewCell: UITableViewCell, UITextFieldDelegate {
     
     @IBOutlet weak var timeFromTextField: CustomTextField!
     @IBOutlet weak var timeToTextField: CustomTextField!
@@ -22,29 +22,21 @@ class WorkingCustomTableViewCell: UITableViewCell, UITextFieldDelegate, UITableV
     @IBOutlet weak var workingClinicErrorTextLabel: UILabel!
     @IBOutlet weak var workingClinicSelectonButton: UIButton!
     @IBOutlet weak var workingListExpandHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var workingClinicSelectionTableView: UITableView!
-    @IBOutlet weak var workingListTableViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var workingTextView: UIView!
     @IBOutlet weak var workingCellRowDelete: UIButton!
     @IBOutlet weak var subView: UIView!
 
-    var string = String.blank
-    var workingListSelection: Bool = false
     var userScheduleTimings: [UserScheduleTimings]?
-    var daysViewModel = WorkingDaysViewModel()
-    var daySelectionArray: [String] = []
-
     var buttoneRemoveDaysTapCallback: () -> ()  = { }
+    weak var delegate: WorkingCellSubclassDelegate?
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        workingClinicSelectionTableView.allowsMultipleSelection = true
         timeFromTextField.tintColor = .clear
         timeToTextField.tintColor = .clear
         timeFromTextField.addInputViewDatePicker(target: self, selector: #selector(doneButtonPressed), mode: .time)
         timeToTextField.addInputViewDatePicker(target: self, selector: #selector(doneButtonPressed1), mode: .time)
-        workingClinicSelectionTableView.register(UINib(nibName: "CustomCell", bundle: nil), forCellReuseIdentifier: "CustomCell")
         workingTextView.layer.cornerRadius = 4.5
         workingTextView.layer.borderWidth = 1
         workingTextView.layer.borderColor = UIColor(red: 204.0/255.0, green: 204.0/255.0, blue: 204.0/255.0, alpha: 1.0).cgColor
@@ -54,13 +46,10 @@ class WorkingCustomTableViewCell: UITableViewCell, UITextFieldDelegate, UITableV
         subView.layer.borderColor = UIColor.init(hexString: "#009EDE").cgColor
     }
     
-    weak var delegate: WorkingCellSubclassDelegate?
-
     override func prepareForReuse() {
         super.prepareForReuse()
         self.delegate = nil
     }
-    
     
     @objc func doneButtonPressed() {
         self.delegate?.buttonWorkingtimeFromTapped(cell: self)
@@ -90,74 +79,5 @@ class WorkingCustomTableViewCell: UITableViewCell, UITextFieldDelegate, UITableV
     
     @IBAction func deleteWorkingButtonAction(sender: UIButton) {
         buttoneRemoveDaysTapCallback()
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return daysViewModel.items.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.identifier, for: indexPath) as? CustomCell {
-            cell.item = daysViewModel.items[indexPath.row]
-            if daysViewModel.items[indexPath.row].isSelected {
-                if !cell.isSelected {
-                    tableView.selectRow(at: indexPath, animated: false, scrollPosition: .none)
-                }
-            } else {
-                if cell.isSelected {
-                    tableView.deselectRow(at: indexPath, animated: false)
-                }
-            }
-            return cell
-        }
-        return UITableViewCell()
-    }
-    
-    internal func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        daysViewModel.items[indexPath.row].isSelected = true
-        if daysViewModel.selectedItems.count == 0 {
-            workingClinicTextLabel.text = Constant.Profile.selectDay
-        } else if daysViewModel.selectedItems.count > 3 {
-            workingClinicTextLabel.text = "\(daysViewModel.selectedItems.count) \(Constant.Profile.days)"
-            workingClinicErrorTextLabel.isHidden = true
-        } else {
-            workingClinicTextLabel.text = daysViewModel.selectedItems.map({$0.title}).joined(separator: ", ")
-            workingClinicErrorTextLabel.isHidden = true
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        daysViewModel.items[indexPath.row].isSelected = false
-        if daysViewModel.selectedItems.count == 0 {
-            workingClinicTextLabel.text = Constant.Profile.selectDay
-        } else if daysViewModel.selectedItems.count > 3 {
-            workingClinicTextLabel.text = "\(daysViewModel.selectedItems.count) \(Constant.Profile.days)"
-            workingClinicErrorTextLabel.isHidden = true
-        } else {
-            workingClinicTextLabel.text = daysViewModel.selectedItems.map({$0.title}).joined(separator: ", ")
-            workingClinicErrorTextLabel.isHidden = true
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 34
-    }
-    
-    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        return tableView.estimatedRowHeight
-    }
-    
-    func workingHideClinicDropDown() {
-        workingListSelection = false
-        workingListTableViewHeightConstraint.constant = 1
-    }
-    
-    func workingShowClinicDropDown() {
-        workingListTableViewHeightConstraint.constant = CGFloat(21 * (daysViewModel.items.count) + 90)
-        workingListSelection = true
     }
 }
