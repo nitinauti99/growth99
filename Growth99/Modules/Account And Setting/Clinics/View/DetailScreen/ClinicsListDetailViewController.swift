0fd39a8b31b066300129c58551c7ca00fbb4274b
Sravan Kumar <bsravankumar549@gmail.com>
2023-01-12 18:54:33 +0530

    Clinic detail added

---
+++ Growth99/Modules/Account And Setting/Clinics/View/DetailScreen/ClinicsListDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ClinicsListDetailViewController.swift
+//  Growth99
+//
+//  Created by Exaze Technologies on 12/01/23.
+//
+
+import UIKit
+
+class ClinicsListDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
