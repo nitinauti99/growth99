6fc4f55b2f554ce1cb51d0a9d286dffedb50cfc7
nitinauti99 <117752778+nitinauti99@users.noreply.github.com>
2023-01-31 19:27:34 +0530

    added IQKeyboredManager

--- Growth99/Application/AppDelegate.swift
+++ Growth99/Application/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var drawerController = KYDrawerController(drawerDirection: .left, drawerWidth: 0.7 * (UIScreen.main.bounds.width))
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.shared.enable = true
         self.setUpTabBarProperty()
         self.window = UIWindow()
         if user.isUserLoged == true {
