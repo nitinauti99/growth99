2b95cde4312a5a3e87ff3b5922ce0abf1e975604
nitinauti99 <nitintech99@gmail.com>
2023-02-16 21:03:00 +0530

    addded form ui

---
+++ Growth99/Modules/FormBuilder/Detail/view/Cell/FormDetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FormDetailTableViewCell.swift
+//  Growth99
+//
+//  Created by Nitin Auti on 16/02/23.
+//
+
+import UIKit
+
+class FormDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
