4e3a76177e0ba0eaad131a6fde951c65abd4095a
Sravan Kumar <nitintech99@gmail.com>
2023-04-01 15:40:12 +0530

    FeedbackQuestionnaire added

--- Growth99/Modules/Appointments/Calender/CalenderView/Model/CalenderModel.swift
+++ Growth99/Modules/Appointments/Calender/CalenderView/Model/CalenderModel.swift
@@ -166,7 +166,7 @@ struct Clinic : Codable {
     let appointmentNotificationReminderDays : Int?
     let appointmentNotificationFeedbackHours : Int?
     let sendRemainderEmail : Bool?
-    let feedbackQuestionnaire : String?
+    let feedbackQuestionnaire : FeedbackQuestionnaire?
     let ccPatientEmail : Bool?
     let showPayButton : Bool?
     let showBookAppointmentButton : Bool?
@@ -190,6 +190,63 @@ struct Clinic : Codable {
     let currencySymbol : String?
     let isReviewPublished : Bool?
     let disableVirtualAppointment : Bool?
+}
+struct FeedbackQuestionnaire : Codable {
+    let createdAt : String?
+    let updatedAt : String?
+    let createdBy : CreatedBy?
+    let updatedBy : UpdatedBy?
+    let deleted : Bool?
+    let tenantId : Int?
+    let id : Int?
+    let defaultQuestionnaireId : Int?
+    let name : String?
+    let isPublic : Bool?
+    let chatQuestionnaire : Bool?
+    let questionnaireSource : String?
+    let isContactForm : String?
+    let isG99ReviewForm : Bool?
+    let isLeadForm : Bool?
+    let showTitle : Bool?
+    let buttonBackgroundColor : String?
+    let buttonForegroundColor : String?
+    let activeSideColor : String?
+    let titleColor : String?
+    let popupTitleColor : String?
+    let popupLabelColor : String?
+    let inputBoxShadowColor : String?
+    let showTextForComposer : Bool?
+    let textForComposer : String?
+    let hideFieldTitle : Bool?
+    let css : String?
+    let isDefault : String?
+    let identifier : String?
+    let isCustom : String?
+    let trackCode : String?
+    let googleAnalyticsGlobalCode : String?
+    let googleAnalyticsGlobalCodeUrl : String?
+    let landingPageName : String?
+    let thankYouPageUrl : String?
+    let appButtonBackgroundColor : String?
+    let appButtonForegroundColor : String?
+    let appTitleColor : String?
+    let submitButtonText : String?
+    let showThankYouPageUrlLinkInContactForm : Bool?
+    let showThankYouPageUrlLinkInVC : Bool?
+    let showThankYouPageUrlLinkInLandingPage : Bool?
+    let thankYouPageUrlVC : String?
+    let thankYouPageUrlLandingPage : String?
+    let configureThankYouMessageInContactForm : Bool?
+    let thankYouPageMessageContactForm : String?
+    let configureThankYouMessageInLandingPage : Bool?
+    let thankYouPageMessageLandingPage : String?
+    let configureThankYouMessageInVC : Bool?
+    let thankYouPageMessageVC : String?
+    let enableModernUi : Bool?
+    let description : String?
+    let subHeading : String?
+    let backgroundImageUrl : String?
+    let showLogo : Bool?
 }
 
 struct Consents : Codable {
