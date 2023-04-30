a1aa6f155c29462be8add74f8572b13e14f06a6b
nitinauti99 <nitintech99@gmail.com>
2023-04-30 22:18:35 +0530

    bug fixes chatBot

---
+++ Growth99/Modules/Chatbot/ChatConfiguration/View/ChatConfigurationViewController+TextFiled.swift
@@ -0,0 +1,66 @@
+//
+//  ChatConfigurationViewController+TextFiled.swift
+//  Growth99
+//
+//  Created by Nitin Auti on 30/04/23.
+//
+
+import Foundation
+import UIKit
+
+extension ChatConfigurationViewController: UITextFieldDelegate  {
+    
+    @IBAction func textFieldDidChange(_ textField: UITextField) {
+        
+        if textField == botName {
+            guard let textField = botName.text, !textField.isEmpty else {
+                botName.showError(message: "Bot Name is required.")
+                return
+            }
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == botName {
+            guard let textField = botName.text, !textField.isEmpty else {
+                botName.showError(message: "Bot Name is required.")
+                return
+             }
+         }
+     }
+}
+
+extension ChatConfigurationViewController: UITextViewDelegate  {
+    
+    func textViewDidChange(_ textField: UITextView) {
+        
+        if textField == defaultWelcomeMessage {
+            guard let textField = defaultWelcomeMessage.text, !textField.isEmpty else {
+                defaultWelcomeMessageLBI.isHidden = false
+                return
+            }
+            self.defaultWelcomeMessageLBI.isHidden = true
+       
+        }else if textField == formMessage {
+            guard let textField = formMessage.text, !textField.isEmpty else {
+                formMessageLBI.isHidden = false
+                return
+            }
+            self.formMessageLBI.isHidden = true
+       
+        }else if textField == welcomeMessage {
+            guard let textField = welcomeMessage.text, !textField.isEmpty else {
+                welcomeMessageLBI.isHidden = false
+                return
+            }
+            self.welcomeMessageLBI.isHidden = true
+      
+        }else if textField == faqNotFoundMessage {
+            guard let textField = faqNotFoundMessage.text, !textField.isEmpty else {
+                faqNotFoundMessageLBI.isHidden = false
+                return
+            }
+            self.faqNotFoundMessageLBI.isHidden = true
+        }
+    }
+}
