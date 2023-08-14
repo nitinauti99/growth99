3da2e6f5ab74880cf80244e861d5ef01f575cc76
nitinauti99 <nitintech99@gmail.com>
2023-08-14 22:00:35 +0530

    pateints create bug fixes

---
+++ Growth99/Modules/Pateints/PateintsAdd/Model/PateintsCreateModel.swift
@@ -0,0 +1,13 @@
+//
+//  PateintsCreateModel.swift
+//  Growth99
+//
+//  Created by Nitin Auti on 14/08/23.
+//
+
+import Foundation
+
+struct PateintsCreateModel: Codable{
+    let message: String?
+    let statusCode: Int?
+}
